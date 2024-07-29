f77c522bda013f962abddb1432a6ea722da3bb27
engineersohag <engineer.sohag.cse@gmail.com>
2024-07-29 22:07:45 +0600

    JS Music Player

---
+++ main.js
@@ -0,0 +1,33 @@
+let progress = document.getElementById('progress');
+let song = document.getElementById('song');
+let ctrlIcon = document.getElementById('ctrlIcon');
+
+song.onloadedmetadata = function(){
+	progress.max = song.duration;
+	progress.value = song.currentTime;
+}
+
+function playPause(){
+	if(ctrlIcon.classList.contains("bx-pause")){
+		song.pause();
+		ctrlIcon.classList.remove("bx-pause");
+		ctrlIcon.classList.add("bx-play");
+	}else{
+		song.play();
+		ctrlIcon.classList.add("bx-pause");
+		ctrlIcon.classList.remove("bx-play");
+	}
+}
+
+if(song.play()){
+	setInterval(() => {
+		progress.value = song.currentTime;
+	},500);
+}
+
+progress.onchange = () => {
+	song.play();
+	song.currentTime = progress.value;
+	ctrlIcon.classList.add("bx-pause");
+	ctrlIcon.classList.remove("bx-play");
+}
